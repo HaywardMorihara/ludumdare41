61bb4435eb9f4524be3a1083654eb8a0175d17fc
ronnie nguyen <itsjustronnie@yahoo.com>
2018-04-21 18:33:34 -0700

    directional animation

--- src/js/penguin.js
+++ src/js/penguin.js
@@ -3,8 +3,15 @@ Penguin = function() {
 
     function init() {
         var penguin = PhaserGame.add.sprite(300, 300, 'penguin');
-        var walk = penguin.animations.add('walk')
+        var penguin_back_walk = penguin.animations.add('penguin_back_walk', [0,1,2,3]);
+        var penguin_front_walk = penguin.animations.add('penguin_front_walk', [4,5,6,7]);
+        var penguin_side_walk = penguin.animations.add('penguin_side_walk', [8,9,10,11]);
+
+        this.direction = "front";
+        this.x_direction = "left";
+
         penguin.scale.setTo(.35,.35);
+        penguin.anchor.setTo(.5,.5);
         PhaserGame.physics.enable(penguin, Phaser.Physics.ARCADE);
         penguin.body.collideWorldBounds = true;
 
@@ -15,10 +22,14 @@ Penguin = function() {
         if (Controller.upKey.isDown)
         {
             penguin.body.velocity.y = -penguinSpeed;
+            penguin.animations.play('penguin_back_walk', 6, true);
+            this.direction = "down";
         }
         else if (Controller.downKey.isDown)
         {
             penguin.body.velocity.y = penguinSpeed;
+            penguin.animations.play('penguin_front_walk', 6, true);
+            this.direction = "up";
         } else 
         {
             penguin.body.velocity.y = 0;
@@ -26,17 +37,29 @@ Penguin = function() {
 
         if (Controller.leftKey.isDown)
         {
+            if(this.x_direction == "right")
+            {
+                penguin.scale.x *= -1;
+            }
             penguin.body.velocity.x = -penguinSpeed;
+            penguin.animations.play('penguin_side_walk', 6, true);
+            this.x_direction = "left";
+            this.direction = this.x_direction;
         }
         else if (Controller.rightKey.isDown)
         {
+            if(this.x_direction == "left")
+            {
+                penguin.scale.x *= -1;
+            }
             penguin.body.velocity.x = penguinSpeed; 
+            penguin.animations.play('penguin_side_walk', 6, true);
+            this.x_direction = "right";
+            this.direction = this.x_direction;
         } else 
         {
             penguin.body.velocity.x = 0;
         }
-
-        penguin.animations.play('walk', 6, true)
         
     }
 
