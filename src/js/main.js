8b086a1b2435acd3c23959e621f1060ef0e13f35
ronnie nguyen <itsjustronnie@yahoo.com>
2018-04-21 18:04:16 -0700

    replace front art

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 var PhaserGame;
 
 function preload() {
-    PhaserGame.load.spritesheet('penguin', 'assets/sprites/penguin-front.png', 200, 200);
+    PhaserGame.load.spritesheet('penguin', 'assets/sprites/penguin.png', 200, 200);
     PhaserGame.load.image('nest', 'assets/static/nest.png');
     PhaserGame.load.image('egg', 'assets/static/diamond.png');
     PhaserGame.load.image('enemy', 'assets/sprites/phaser-dude.png');
