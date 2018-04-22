5860a8055d0397f9ae4d1770dfda92beeebd1f8d
ronnie nguyen <itsjustronnie@yahoo.com>
2018-04-21 20:48:38 -0700

    colored penguins in multiplayer mode

--- src/js/main.js
+++ src/js/main.js
@@ -13,7 +13,10 @@ State = {
 };
 
 function preload() {
-    PhaserGame.load.spritesheet('penguin', 'assets/sprites/penguin.png', 200, 200);
+    PhaserGame.load.spritesheet('penguin-black', 'assets/sprites/penguin.png', 200, 200);
+    PhaserGame.load.spritesheet('penguin-orange', 'assets/sprites/penguin-orange.png', 200, 200);
+    PhaserGame.load.spritesheet('penguin-blue', 'assets/sprites/penguin-blue.png', 200, 200);
+    PhaserGame.load.spritesheet('penguin-pink', 'assets/sprites/penguin-pink.png', 200, 200);
     PhaserGame.load.image('nest', 'assets/static/nest.png');
     PhaserGame.load.image('egg', 'assets/static/diamond.png');
     PhaserGame.load.image('enemy', 'assets/sprites/phaser-dude.png');
