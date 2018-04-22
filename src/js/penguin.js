1b2e712bd55abacb8bd5e60334a1c16be701075a
Michelle Doelling <michelle.doelling@ANYUSEACTUAL.workdayinternal.com>
2018-04-21 19:22:35 -0700

    removed redundant direction strings

--- src/js/penguin.js
+++ src/js/penguin.js
@@ -7,7 +7,7 @@ Penguin = function() {
         var penguin_front_walk = penguin.animations.add('penguin_front_walk', [4,5,6,7]);
         var penguin_side_walk = penguin.animations.add('penguin_side_walk', [8,9,10,11]);
 
-        this.direction = "front";
+        this.direction = DirectionEnum.DOWN;
         this.x_direction = "left";
 
         penguin.scale.setTo(.35,.35);
@@ -24,14 +24,12 @@ Penguin = function() {
             penguin.body.velocity.y = -penguinSpeed;
             penguin.direction = DirectionEnum.UP;
             penguin.animations.play('penguin_back_walk', 6, true);
-            this.direction = "down";
         }
         else if (Controller.downKey.isDown)
         {
             penguin.body.velocity.y = penguinSpeed;
             penguin.direction = DirectionEnum.DOWN;
             penguin.animations.play('penguin_front_walk', 6, true);
-            this.direction = "up";
         } else 
         {
             penguin.body.velocity.y = 0;
@@ -47,7 +45,6 @@ Penguin = function() {
             penguin.direction = DirectionEnum.LEFT;
             penguin.animations.play('penguin_side_walk', 6, true);
             this.x_direction = "left";
-            this.direction = this.x_direction;
         }
         else if (Controller.rightKey.isDown)
         {
@@ -59,7 +56,6 @@ Penguin = function() {
             penguin.direction = DirectionEnum.RIGHT;
             penguin.animations.play('penguin_side_walk', 6, true);
             this.x_direction = "right";
-            this.direction = this.x_direction;
         } else 
         {
             penguin.body.velocity.x = 0;
